9c1231935611c5893c6626e9b3702b6fd2d819d3
Jess Daniel <jessdaniel474@gmail.com>
2019-11-11 17:53:16 -0500

    added tests in controls and display components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,31 @@
 // Test away!
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("it renders correctly", () => {
+  expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test("toggleClosed toggles between open and closed", () => {
+  const toggleClosedMock = jest.fn();
+  const { getByText } = render(<Controls toggleClosed={toggleClosedMock} />);
+  const closeButton = getByText(/close gate/i);
+  fireEvent.click(closeButton);
+  expect(toggleClosedMock).toHaveBeenCalled();
+});
+
+// test("toggleLocked toggles between locked and unlocked", async () => {
+//   const toggleClosedMock = jest.fn();
+//   const toggleLockedMock = jest.fn();
+//   const { getByText } = render(
+//     <Controls toggleLocked={toggleLockedMock} toggleClosed={toggleClosedMock} />
+//   );
+//   const closeButton = getByText(/close gate/i);
+//   await fireEvent.click(closeButton);
+//   expect(toggleClosedMock).toHaveBeenCalled();
+//   const lockedButton = getByText(/lock gate/i);
+//   fireEvent.click(lockedButton);
+//   expect(toggleLockedMock).toHaveBeenCalled();
+// });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+test("Display renders properly", () => {
+  expect(render(<Display />)).toMatchSnapshot();
+});
+
+test("default gate is open", () => {
+  const { getByText } = render(<Display />);
+  getByText(/open/i);
+});
+
+test("Gate cganges to close", () => {
+  const { getByText } = render(<Display />);
+  getByText(/open/i);
+});
